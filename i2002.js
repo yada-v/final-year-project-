269c08ea36d6300f1efbff94af53bcfbc451795d
yada-v <yadavkraj555@gmail.com>
2026-04-03 21:17:59 +0530

    Add files via upload

---
+++ i2002.js
@@ -0,0 +1,9 @@
+let slider = document.getElementById("slider")
+function update(){
+let t = parseFloat(slider.value);                          
+document.getElementById("real").style.opacity= 1 - t ; 
+document.getElementById("ideal").style.opacity=t;   
+
+							   }                                                                                                                   
+update();                                           
+slider.addEventListener("input",update);
---
+++ i2004.js
@@ -0,0 +1,42 @@
+
+let realSketch = function(p){
+
+let lines = [];
+p.setup = function(){
+let rightDiv= document.getElementById("real");
+let canvas=p.createCanvas(rightDiv.clientWidth,rightDiv.clientHeight);  canvas.parent("real");
+p.textAlign(p.CENTER,p.CENTER)
+for(let i = 0;i<100;i++){
+        lines.push({
+x: p.random(p.width),
+y: p.random(p.height),
+len: p.random(20,50),
+speed: p.random(10,20)
+        });
+}
+}
+
+let dropx;
+let dropy;
+
+
+
+p.draw =function(){
+p.background(80,160,200);
+
+p.textSize(64);
+p.text("💧" , p.width/2, p.height/2)
+for(let l of lines){
+p.stroke(255);
+p.strokeWeight(2);
+p.line(l.x,l.y,l.x,l.y+l.len);
+l.y -= l.speed;
+
+        if(l.y<0){l.y=p.height;}
+
+}
+};
+};
+new p5(realSketch);
+
+
